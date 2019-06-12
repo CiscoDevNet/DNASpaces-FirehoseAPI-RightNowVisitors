d32c2ec8a1c81594c4289610ea1796cfd98f878a
thangs <thangs@cisco.com>
2019-06-12 17:39:40 +0530

    Import organised

--- server/src/main/java/com/cisco/dnaspaces/db/Redshift.java
+++ server/src/main/java/com/cisco/dnaspaces/db/Redshift.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
